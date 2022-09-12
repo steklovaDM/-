ab774370d83da34ad168419884f025a2ed447ad6
steklovaDM <108463196+steklovaDM@users.noreply.github.com>
2022-09-12 09:22:13 +0300

    Add files via upload
    Добавила файл

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+ 
+string[] array =
+{
+    "Я", "ты", "вместе","он", "много", "она"
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+
